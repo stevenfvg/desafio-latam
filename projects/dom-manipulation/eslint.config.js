2a08c316257cdcc9d93060162bfb1c1bfb6baa79
Steven Valladares <stevenvalladaresgiron@gmail.com>
2026-01-25 17:10:27 -0300

    build(projects): :heavy_plus_sign: install and configure ESLint for syntax checking and error handling

---
+++ projects/dom-manipulation/eslint.config.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        rules: {
+            semi: ["error", "always"],
+        }
+    }
+];
